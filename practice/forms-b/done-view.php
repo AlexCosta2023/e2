b2db5b2f327213072f9ca0d5540705a3f72a7dd4
Alex Costa <alex.costa.a@gmail.com>
2023-10-19 12:36:13 +0000

    e2 Practice (Week 6) Class work

---
+++ practice/forms-b/done-view.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang='en'>
+<head>
+    <title>Forms B - Results</title>
+    <meta charset='utf-8'>
+    <link href=data: , rel=icon>
+</head>
+<body>
+ 
+    <h1>Results</h1>
+
+    <?php if ($haveAnswer == false) { ?>
+        Please enter an answer. [<a href='index.php'>Please try again</a>]
+    <?php } elseif ($correct) { ?>
+        You got it correct!
+    <?php } else { ?>
+        Incorrect. [<a href='index.php'>Please try again</a>]
+    <?php } ?>
+</body>
+</html>
